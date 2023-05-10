2a7a9826208e3fbf1a2abe4c4146c65f28e4fed5
yumin0531 <yumin060531@gmail.com>
2023-05-10 19:50:22 +0900

    fix: bug fix

--- bubblebubble.py
+++ bubblebubble.py
@@ -3,10 +3,10 @@ import random
 
 pygame.init()
 
-#화면 크기 설정 할거임
 screen_width = 800
 screen_height = 500
 pygame.display.set_mode((screen_width, screen_height))
 
-# 화면 타이틀 설정
 pygame.display.set_caption("bubblebobble")
+
+running = False
