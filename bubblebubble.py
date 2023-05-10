b2d982bb3050738ffa3744f21691fb37889af369
yumin0531 <yumin060531@gmail.com>
2023-05-10 19:39:23 +0900

    init

---
+++ bubblebubble.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+
+pygame.init()
+
+#화면 크기 설정 할거임
+screen_width = 800
+screen_height = 500
+pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("bubblebobble")
